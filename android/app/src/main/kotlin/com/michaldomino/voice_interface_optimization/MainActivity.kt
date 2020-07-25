52a2747afd0d323f970459a4e1aa324cade5adf5
Michał Domino <micdom9@gmail.com>
2020-07-25 16:28:56 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/michaldomino/voice_interface_optimization/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.michaldomino.voice_interface_optimization
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}

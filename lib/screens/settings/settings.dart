358e77a416637b9572028e4cf681de0a5f28a1b3
Michał Domino <leahcim333@o2.pl>
2020-08-16 13:07:06 +0200

    Add rebuilding app only when language changes

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 import 'package:voice_interface_optimization/blocs/localization/localization_bloc.dart';
 import 'package:voice_interface_optimization/generated/l10n.dart';
 
 class Settings extends StatefulWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("Settings"),
-//      ),
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: (context) {_changeLanguage(context)},
-//        tooltip: 'Increment',
-//        child: Icon(Icons.add),
-//      ),
-//    );
-//  }
 
   @override
   _SettingsState createState() => _SettingsState();
@@ -30,27 +16,37 @@ class _SettingsState extends State<Settings> {
     return BlocBuilder<LocalizationBloc, LocalizationState>(
       builder: (context, state) {
         return Scaffold(
-          appBar: AppBar(
-            title: Text(S.of(context).settings),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => _changeLanguage(context),
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-        );
+            appBar: AppBar(
+              title: Text(S.of(context).settings),
+            ),
+            floatingActionButton: Row(
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: () => _changeLanguage(context, 'en'),
+                  tooltip: 'en',
+                  child: Icon(Icons.add),
+                  heroTag: null,
+                ),
+                FloatingActionButton(
+                  onPressed: () => _changeLanguage(context, 'pl'),
+                  tooltip: 'pl',
+                  child: Icon(Icons.g_translate),
+                  heroTag: null,
+                )
+              ],
+            ));
       },
     );
   }
 
-  void _changeLanguage(BuildContext context) {
+  void _changeLanguage(BuildContext context, String targetLanguageCode) {
     setState(() {
-      String targetLanguageCode;
-      if (Intl.defaultLocale == 'en') {
-        targetLanguageCode = 'pl';
-      } else {
-        targetLanguageCode = 'en';
-      }
+//      String targetLanguageCode;
+//      if (Intl.defaultLocale == 'en') {
+//        targetLanguageCode = 'pl';
+//      } else {
+//        targetLanguageCode = 'en';
+//      }
       BlocProvider.of<LocalizationBloc>(context)
           .add(ChangeLanguageEvent(context, Locale(targetLanguageCode)));
     });

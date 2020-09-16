a5929884dea1e4bb3dc22a88379f46be7a377eae
Michał Domino <leahcim333@o2.pl>
2020-09-16 12:26:07 +0200

    Moved common part of custom_text_speaking_screen.dart and predefined_text_speaking_screen.dart to text_speaking.dart

--- lib/screens/text_speaking/custom_text_speaking/custom_text_speaking_screen.dart
+++ lib/screens/text_speaking/custom_text_speaking/custom_text_speaking_screen.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:voice_interface_optimization/blocs/localization/localization_cubit.dart';
-import 'package:voice_interface_optimization/logic/speaker.dart';
-import 'package:voice_interface_optimization/screens/reusable/appbar.dart';
-import 'package:voice_interface_optimization/screens/text_speaking/speaker_regulator.dart';
+import 'package:voice_interface_optimization/screens/text_speaking/text_speaking.dart';
 
 class CustomTextSpeakingScreen extends StatefulWidget {
   CustomTextSpeakingScreen({Key key, this.title}) : super(key: key);
@@ -16,64 +12,20 @@ class CustomTextSpeakingScreen extends StatefulWidget {
 }
 
 class _CustomTextSpeakingScreenState extends State<CustomTextSpeakingScreen> {
-  Speaker _speaker;
-  String _textToSpeak;
-
-  @override
-  void initState() {
-    super.initState();
-    _speaker = Speaker();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _speaker.dispose();
-  }
+  String _currentText;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<LocalizationCubit, LocalizationState>(
-        builder: (context, state) {
-      return Scaffold(
-        appBar: CustomAppbar(context).get(),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    padding: EdgeInsets.only(left: 25.0, right: 25.0),
-                    child: TextField(
-                      onChanged: (String value) {
-                        setState(() {
-                          _textToSpeak = value;
-                        });
-                      },
-                    )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    InkWell(
-                      child: Icon(
-                        Icons.play_arrow,
-                        color: Colors.green,
-                        size: 50,
-                      ),
-                      onTap: _playSound,
-                    ),
-                  ],
-                ),
-                SpeakerRegulator(_speaker),
-              ],
-            ),
-          ),
-        ),
-      );
-    });
-  }
-
-  _playSound() async {
-    _speaker.speak(_textToSpeak);
+    return TextSpeaking(
+      currentText: _currentText,
+      textToSpeakAccessor: () => _currentText,
+      textInput: TextField(
+        onChanged: (String value) {
+          setState(() {
+            _currentText = value;
+          });
+        },
+      ),
+    );
   }
 }
--- lib/screens/text_speaking/predefined_texts_speaking/predefined_text_speaking_screen.dart
+++ lib/screens/text_speaking/predefined_texts_speaking/predefined_text_speaking_screen.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:voice_interface_optimization/blocs/localization/localization_cubit.dart';
 import 'package:voice_interface_optimization/blocs/texts_language/texts_language_cubit.dart';
 import 'package:voice_interface_optimization/logic/predefined_texts.dart';
-import 'package:voice_interface_optimization/logic/speaker.dart';
-import 'package:voice_interface_optimization/screens/reusable/appbar.dart';
-import 'package:voice_interface_optimization/screens/text_speaking/speaker_regulator.dart';
+import 'package:voice_interface_optimization/screens/text_speaking/text_speaking.dart';
 
 class PredefinedTextSpeakingScreen extends StatefulWidget {
   @override
@@ -15,87 +12,36 @@ class PredefinedTextSpeakingScreen extends StatefulWidget {
 
 class _PredefinedTextSpeakingScreenState
     extends State<PredefinedTextSpeakingScreen> {
-  Speaker _speaker;
-  String _textToSpeak;
-
-  @override
-  initState() {
-    super.initState();
-    _speaker = Speaker();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _speaker.dispose();
-  }
+  String _currentText;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<LocalizationCubit, LocalizationState>(
-        builder: (context, state) {
-      return Scaffold(
-        appBar: CustomAppbar(context).get(),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    padding: EdgeInsets.only(left: 25.0, right: 25.0),
-                    child: BlocBuilder<TextsLanguageCubit, TextsLanguageState>(
-                        builder: (context, state) {
-                      return DropdownButton(
-                        isExpanded: true,
-                        value: _textToSpeak,
-                        items: PredefinedTexts()
-                            .texts
-                            .entries
-                            .map((e) => DropdownMenuItem(
-                                  value: e.key,
-                                  child: FittedBox(
-                                    child: Text(e.value),
-                                    fit: BoxFit.contain,
-                                  ),
-                                ))
-                            .toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            _textToSpeak = value;
-                          });
-                        },
-                      );
-                    })),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    InkWell(
-                      child: Icon(
-                        Icons.play_arrow,
-                        color: Colors.green,
-                        size: 50,
-                      ),
-                      onTap: _playSound,
+    return TextSpeaking(
+      currentText: _currentText,
+      textToSpeakAccessor: () => PredefinedTexts().texts[_currentText],
+      textInput: BlocBuilder<TextsLanguageCubit, TextsLanguageState>(
+          builder: (context, state) {
+        return DropdownButton(
+          isExpanded: true,
+          value: _currentText,
+          items: PredefinedTexts()
+              .texts
+              .entries
+              .map((e) => DropdownMenuItem(
+                    value: e.key,
+                    child: FittedBox(
+                      child: Text(e.value),
+                      fit: BoxFit.contain,
                     ),
-                  ],
-                ),
-                SpeakerRegulator(_speaker),
-              ],
-            ),
-          ),
-        ),
-      );
-    });
-  }
-
-  // Future _loadTextsList() async {
-  //   PredefinedTexts predefinedTexts =
-  //       await PredefinedTexts.loadFromLanguageCode(
-  //           TextsLanguage().textLanguage);
-  //   _texts = predefinedTexts.texts;
-  // }
-
-  _playSound() async {
-    _speaker.speak(_textToSpeak);
+                  ))
+              .toList(),
+          onChanged: (value) {
+            setState(() {
+              _currentText = value;
+            });
+          },
+        );
+      }),
+    );
   }
 }

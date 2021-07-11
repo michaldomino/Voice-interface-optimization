be178b5844b6d78b853b86a1667c1216128c655c
Michał Domino <michal.domino@yahoo.com>
2021-07-11 17:41:01 +0200

    Fixed not sending results after changing texts language in stt tests

--- lib/blocs/stt_tests/stt_tests_cubit.dart
+++ lib/blocs/stt_tests/stt_tests_cubit.dart
@@ -93,8 +93,18 @@ class SttTestsCubit extends Cubit<SttTestsState> {
   Future sendResults(List<SttTestResult> sttTestResults) async {
     AuthenticationState authenticationState = _authenticationCubit.state;
     if (authenticationState is AuthenticationAuthenticated) {
-      await _sttTestsService.postSttTestResults(
+      var response = await _sttTestsService.postSttTestResults(
           sttTestResults, authenticationState.token.access);
+      if (response.statusCode == HttpStatus.unauthorized) {
+        await _authenticationCubit
+            .refreshToken(authenticationState.token.refresh);
+        AuthenticationState authenticationStateRefreshed =
+            _authenticationCubit.state;
+        if (authenticationStateRefreshed is AuthenticationAuthenticated) {
+          await _sttTestsService.postSttTestResults(
+              sttTestResults, authenticationState.token.access);
+        }
+      }
     }
   }
 
